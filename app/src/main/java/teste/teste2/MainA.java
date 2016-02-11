817a42d8d326e9ed00689fb626ed31761d8ee545
Abel Gehm <abelgehm@gmail.com>
2016-02-11 15:16:59 -0200

    Documentação/Alinhamento/Visual
    Melhorias na documentação
    Remoção do menu não utilizado
    Ajuste do alinhamento dos componentes
    Bloqueio de endereços externos ao servidor web

--- app/src/main/java/teste/teste2/MainA.java
+++ app/src/main/java/teste/teste2/MainA.java
@@ -1,5 +1,5 @@
 package teste.teste2;
-
+// Bibliotecas
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
@@ -16,62 +16,73 @@ import android.preference.PreferenceManager;
 
 public class MainA extends ActionBarActivity {
 
+    // Interface com o botão da interface principal
     private Button btnAbrirInterface;
+    // Interface com a caixa de texto apikey da interface principal
     private EditText APIKEY;
+    // Nome do servidor fornecido pelo operador retirado da interface pricipal
     private String ApiKey = "";
+    // Interface com a caixa de texto endereço do seridor da interface principal
     private EditText Servidor;
+    // Endereço do servidor fornecido pelo operador
     public static String servidor = "";
-
+    // Tela do navegador web embutido no aplicativo
     private WebView wvTelaOnline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Cria uma interface entre os objetos da interface principal e os objetos manipulados pelo código
         APIKEY = (EditText) findViewById(R.id.txtApiKey);
         Servidor = (EditText)findViewById(R.id.txtEndereco);
         btnAbrirInterface = (Button) findViewById(R.id.btnAbrir);
-
-        SharedPreferences Opcoes = this.getPreferences(MODE_PRIVATE);
-
-        servidor = Opcoes.getString("servidor", "");
-        ApiKey = Opcoes.getString("apikey", "");
-
-        Servidor.setText(servidor);
-        APIKEY.setText(ApiKey);
         wvTelaOnline = (WebView) findViewById(R.id.wvOnline);
 
+        // Inteface com o sistema de configurações
+        SharedPreferences Opcoes = this.getPreferences(MODE_PRIVATE);
+        // Carrega as configurações
+        servidor = Opcoes.getString("servidor", "");
+        ApiKey = Opcoes.getString("apikey", "");
+        // Sincroniza as configurações com a interface gráfica
+        Servidor.setText(servidor);
+        APIKEY.setText(ApiKey);
+
+        // Define a rotina do botão "abrir interface"
         btnAbrirInterface.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Endereço completo = servidor + script.php + argumentos (GET)
                 String EnderecoCompleto = "";
+                // Sincroniza novamente os elementos da interface principal com os objetos do código
                 servidor = Servidor.getText().toString();
                 ApiKey = APIKEY.getText().toString();
 
                 // Salva as configuracoes
                 SharedPreferences Opcoes = getPreferences(MODE_PRIVATE);
                 SharedPreferences.Editor Editor = Opcoes.edit();
+
                 Editor.putString("servidor", servidor);
                 Editor.putString("apikey", ApiKey);
-
                 Editor.commit();
 
 
-                // Coloca a "/" no final se precisar
+                // Coloca a "/" no final se precisar (servidor/)
                 if (servidor.charAt(servidor.length() - 1) != '/'){
                     servidor += "/";
                 }
-                // Coloca "http://" no inicio, se precisar
+                // Coloca "http://" no inicio, se precisar (http://servidor/)
                 if (!servidor.startsWith("http")){
                     servidor = "http://" + servidor;
                 }
+                // http://servidor/script.php?apikey=xxxx
                 EnderecoCompleto = servidor + "mobile.php?apikey=" + ApiKey;
-                ApiKey = APIKEY.getText().toString();
 
-                // Force links and redirects to open in the WebView instead of in a browser
+                // Os endereços serão abertos no webView e não no navegador
                 wvTelaOnline.setWebViewClient(new WebViewClient());
 
-                // Enable Javascript
+                // Permite javascript (não tem javascript no script php atual)
                 WebSettings ws = wvTelaOnline.getSettings();
                 ws.setJavaScriptEnabled(true);
 
@@ -82,37 +93,31 @@ public class MainA extends ActionBarActivity {
                 wvTelaOnline.loadUrl(EnderecoCompleto);
 
                 // Redireciona ao webBrowser links para outras páginas
-                // wvTelaOnline.setWebViewClient(new myWebView());
-
-
-                // btnAbrirInterface.setVisibility(View.GONE);
-
-                // btnAbrirInterface.setText(servidor);
-                // Abrir o webview:
+                wvTelaOnline.setWebViewClient(new myWebView());
             }
         });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        //return true;
+    //}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+            //return true;
+        //}
 
-        return super.onOptionsItemSelected(item);
-    }
+        //return super.onOptionsItemSelected(item);
+    //}
 }
--- app/src/main/java/teste/teste2/myWebView.java
+++ app/src/main/java/teste/teste2/myWebView.java
@@ -4,14 +4,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import teste.teste2.MainA;
+
 /**
  * Created by A1 on 10/02/2016.
  */
 public class myWebView extends WebViewClient{
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (Uri.parse(url).getHost().endsWith(MainA.servidor)) {
+        //if (Uri.parse(url).getHost().endsWith(MainA.servidor)) {
+        if (Uri.parse(url).getHost().startsWith(MainA.servidor)){
 
             return false;
         }
